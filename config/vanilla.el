395ab418f900c4c6e4f1236665a97f96bc0ce6c0
Patrick Aldis <patrick.aldis@virginmedia.com>
2025-04-16 22:29:11 +0100

    use backticks

--- config/vanilla.el
+++ config/vanilla.el
@@ -1,7 +1,7 @@
 ;; BACKUPS
 (setq
-    backup-directory-alist '(("." . "~/.emacs.d/backup"))
-    auto-save-file-name-transforms `((".*" "~/.emacs.d/auto-saves/" t))
+    backup-directory-alist '(("." . "~/.emacs.d/backup/"))
+    auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-saves/" t))
     backup-by-copying t    ; Don't delink hardlinks
     version-control t      ; Use version numbers on backups
     delete-old-versions t  ; Automatically delete excess backups
